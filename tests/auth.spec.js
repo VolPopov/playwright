bf9e4b4af5e3801fd0fa3e78d660e988dd8c3e28
VolPopov <volodjapopov01@gmail.com>
2025-02-06 18:19:21 +0100

    Correct code

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,43 +1,32 @@
 import { test, expect } from '@playwright/test';
-import { generateUserCredentials, HEADINGS, URLS, utils } from '../fixtures';
+import { HEADINGS, URLS, generateUserCredentials, utils } from '../fixtures';
 
 test.describe.configure({ mode: 'serial' });
-
-test.describe('Register a user and log in', () => {
-  const { username, email, password } = generateUserCredentials(7);
+test.describe('register a user and log in', () => {
+  const { username, email, password } = generateUserCredentials(5);
 
   test('register with valid data', async ({ page }) => {
-    /* steps:
-    visit register page
-    fill in the form
-    click on submit button
-    */
+    // visit the register page
     await page.goto(URLS['REGISTER']);
 
     await expect(page.locator('h1')).toBeVisible();
     await expect(page.locator('h1')).toHaveText(HEADINGS['REGISTER']);
 
-    // await page.locator('#username').fill(username);
-    // await page.locator('#email').fill(email);
-    // await page.locator('#password').fill(password);
-    // await page.locator('button').click();
+    // fill in the form
     utils.fillAndSubmitForm(page, 'input', [username, email, password]);
+
+    await expect(page).toHaveURL(URLS['DASHBOARD']);
   });
 
-  test('login with registered user', async ({ page }) => {
-    /* steps:
-    visit login page
-    fill in the form
-    click on submit button
-    */
+  test('log in with registered user', async ({ page }) => {
+    // visit the login page
     await page.goto(URLS['LOGIN']);
 
     await expect(page.locator('h1')).toBeVisible();
     await expect(page.locator('h1')).toHaveText(HEADINGS['LOGIN']);
 
-    await page.locator('#email').fill(email);
-    await page.locator('#password').fill(password);
-    await page.locator('button').click();
+    // fill in the form
+    utils.fillAndSubmitForm(page, 'input', [email, password]);
 
     await expect(page).toHaveURL(URLS['DASHBOARD']);
   });
