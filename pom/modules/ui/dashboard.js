0bdefb344aa3dc186696c66a3bfd6dab537a06d8
VolPopov <volodjapopov01@gmail.com>
2025-02-15 19:01:32 +0100

    Dashboard tests started

--- pom/modules/ui/dashboard.js
+++ pom/modules/ui/dashboard.js
@@ -9,12 +9,21 @@ export class Dashboard {
     this.cartMenu = page.locator(
       "section[class='flex-1 overflow-y-auto px-2 py-2']"
     );
-    this.textOfItem = this.cartMenu.locator(
-      "[class='flex-1 overflow-y-auto px-2 py-2'] > div"
-    );
+  
+    this.itemsInCart = page.locator("div[class='flex align-middle w-full max-w-lg']");
+    this.nameOfProduct = this.itemsInCart.locator("div");
+    this.removeItemButton = this.itemsInCart.locator("button");
+    this.cartUpperMenu = page.locator("section[class='h-fit w-full text-center relative']");
+    this.clearButton = this.cartUpperMenu.locator("button");
+    this.emptyCart = this.cartMenu.locator("div:has-text('No items in cart. Add some!')");
   }
 
   async addAnItemToCart(broj) {
     await this.addToCartButton.nth(broj).click();
   }
+
+  async deleteAllItemsFromCart() {
+    await this.clearButton.click();
+  }
+  
 }
